8a284217ea02d93993270d3f1d9fb9427605969e
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-11-15 12:55:18 -0800

    added still/animate functionality to the populated giphys

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    // To Do 
+    // - Make the search into random endpoint
+    // - When user clicks on image, change src to the moving giphy
+    // - When user clicks on moving image, change src to still giphy
+
+    // - User Input Box
+    // - When text is input the box, it adds a functioning button to topics array
+
+
 
     // Example queryURL for Giphy API
     // var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=nPsGacEw599ZV3kh8FmX95ZE8WEXSxha&q=dinosaur&limit=10&offset=0&rating=PG-13&lang=en";
@@ -11,7 +20,7 @@ $(document).ready(function () {
     //API
 
     // Topic is food!
-    var topics = ["Pizza", "Tacos", "Burritos", "Pineapples", "Cat Food", "Croissants", "Burgers", "Tide Pods",];
+    var topics = ["Pizza", "Tacos", "Burritos", "Pineapples", "Cat Food", "Croissants", "Burgers", "Cats", "Tide Pods"];
 
     // Limit results to not break computer memory lmaooo
     var limit = 10;
@@ -47,8 +56,14 @@ $(document).ready(function () {
         $.ajax({
             // test random endpoint https://api.giphy.com/v1/gifs/random?api_key=nPsGacEw599ZV3kh8FmX95ZE8WEXSxha&tag=&rating=PG 
             // api is http://api.giphy.com/v1/gifs/search?q=food&api_key=nPsGacEw599ZV3kh8FmX95ZE8WEXSxha&rating=pg&limit=1 
+            // url: "http://api.giphy.com/v1/gifs/search?q=" + food +
+            //     "&api_key=nPsGacEw599ZV3kh8FmX95ZE8WEXSxha&rating=" + rating + "&limit=" + limit,
+
+            // Working API, 10 limit, rating PG
             url: "http://api.giphy.com/v1/gifs/search?q=" + food +
                 "&api_key=nPsGacEw599ZV3kh8FmX95ZE8WEXSxha&rating=" + rating + "&limit=" + limit,
+            // url: "https://api.giphy.com/v1/gifs/random?api_key=nPsGacEw599ZV3kh8FmX95ZE8WEXSxha&tag=" + food + "&rating=PG",
+
             // test random endpoint
             // url: "https://api.giphy.com/v1/gifs/random?q=" + food +
             //     "&api_key=nPsGacEw599ZV3kh8FmX95ZE8WEXSxha&rating=" + rating + "&limit=" + limit,
@@ -56,21 +71,43 @@ $(document).ready(function () {
 
             method: "GET"
         }).then(function (response) {
-
+            // test console
+            console.log("it works");
             response.data.forEach(function (element) {
                 newDiv = $("<div>");
                 newDiv.addClass("individual-gif-container");
                 newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
 
-                // Displays image to HTML 
+                // Displays still image to HTML 
                 var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
                 newImage.addClass("gif-image");
+
+                // Adds still animate and still attributes to giphy so we can use clicks to change states
+                newImage.attr("state", "still");
+                newImage.attr("data-still", element.images.fixed_height_still.url);
+                newImage.attr("data-animate", element.images.fixed_height.url);
                 newDiv.append(newImage);
                 $("#gif-box").append(newDiv);
 
                 // console.log("ajax works");
             });
 
+            // Animate and still of the giphy using clicks
+            $(".gif-image").on("click", function () {
+
+                // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+                var state = $(this).attr("data-state");
+                // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+                // Then, set the image's data-state to animate
+                // Else set src to the data-still value
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
         });
     }
     // End populate gif box function 
